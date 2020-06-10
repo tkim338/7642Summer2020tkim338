499def64d8402b75742b81db690d7aa575265730
Thomas Kim <tkim338@gatech.edu>
2020-06-10 01:50:30 -0400

    semi working hw4

--- Other/HW4/hw4.py
+++ Other/HW4/hw4.py
@@ -1,63 +1,91 @@
 import numpy
 import gym
+import time
 
 
-def optimal_action(table, state):
-    max_reward = 0
-    best_action = 0
-    for action in range(len(table[state])):  # [0, 1, 2, 3]
-        reward = table[state][action]
-        if reward > max_reward:
+def compute_reward(state, action):
+    # - 0: move south
+    # - 1: move north
+    # - 2: move east
+    # - 3: move west
+    # - 4: pickup passenger
+    # - 5: dropoff passenger
+    return
+
+
+# def optimal_action(environment):
+#     max_reward = None
+#     possible_actions = []
+#     action_list = environment.P[environment.s]
+#     for action in range(environment.nA):  # [0, 1, 2, 3, 4, 5]
+#         reward = action_list[action][0][2]
+#         if max_reward is None or reward > max_reward:
+#             max_reward = reward
+#             possible_actions = [action]
+#         elif reward == max_reward:
+#             possible_actions.append(action)
+#     return possible_actions[numpy.random.randint(0, len(possible_actions))]
+
+
+def optimal_action(environment, policy):
+    max_reward = None
+    possible_actions = []
+    action_list = environment.P[environment.s]
+    for action in range(environment.nA):  # [0, 1, 2, 3, 4, 5]
+        reward = action_list[action][0][2]
+        if max_reward is None or reward > max_reward:
+            max_reward = reward
+            possible_actions = [action]
+        elif reward == max_reward:
+            possible_actions.append(action)
+    return possible_actions[numpy.random.randint(0, len(possible_actions))]
+
+
+def policy_action(environment, policy):
+    max_reward = None
+    best_action = None
+    for action in range(environment.nA):  # [0, 1, 2, 3, 4, 5]
+        reward = policy[environment.s][action]
+        if max_reward is None or reward > max_reward:
             max_reward = reward
             best_action = action
     return best_action
 
 
-def get_action(table, state, eps):
+def get_action(environment, policy, eps):
     a = numpy.random.random()
     if a < eps:
-        return numpy.random.randint(4)
+        return numpy.random.randint(environment.nA)
     else:
-        return optimal_action(table, state)
+        return policy_action(environment, policy)
 
-
-def print_policy(table):
-    mapping = {0: '<', 1: 'v', 2: '>', 3: '^'}
-    output_string = ''
-    for state in range(len(table)):
-        best_action = optimal_action(table, state)
-        output_string += mapping[best_action]
-    return output_string
 
 # input params
-amap="SFFG"
-gamma=0.95
-alpha=0.1
-epsilon=0.23
-episodes=18804
-seed=105338
+gamma = 0.9
+alpha = 0.4
+epsilon = 0.2
+episodes = 10000000
 
 # setup
-# numpy.random.seed(seed)
-# desc = generate_map(amap)
 env = gym.make('Taxi-v3').unwrapped
-# env.seed(seed)
+pol = numpy.zeros((env.nS, env.nA))
+
+for ep in range(episodes):
+    env.reset()
+    # env.render()
+    rew = 0
+    next_action = get_action(env, pol, epsilon)
+    don = False
+    while not don and rew > -20:
+        prev_s = env.s
+        prev_action = next_action
+        ob, rew, don, info = env.step(next_action)  # act(epsilon, env, next_action)
+        next_action = get_action(env, pol, epsilon)
+
+        #env.render()
+        # update policy[state][action]
+        pol[prev_s][prev_action] += alpha*(rew + gamma*pol[env.s][get_action(env, pol, 0.0)] - pol[prev_s][prev_action])
+        #time.sleep(0.1)
+
 env.render()
-# q_table = numpy.zeros((len(amap), 4))  # [[0] * 4] * len(amap)
-#
-# for ep in range(episodes):
-#     ob = env.reset()
-#     rew = 0
-#     next_action = get_action(q_table, ob, epsilon)
-#     don = False
-#     while not don:
-#         prev_ob = ob  # ob = state
-#         prev_action = next_action
-#         ob, rew, don, info = env.step(next_action)  # act(epsilon, env, next_action)
-#         next_action = get_action(q_table, ob, epsilon)
-#
-#         # update q_table[state][action]
-#         q_table[prev_ob][prev_action] += alpha*(rew + gamma*q_table[ob][next_action] - q_table[prev_ob][prev_action])
-#
-# print(q_table)
-# print(print_policy(q_table))
+print('done')
